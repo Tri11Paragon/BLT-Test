29d69deb010ad6b5e59d7c66e96de1e6cc5be505
Brett Laptop <tri11paragon@tpgc.me>
2023-07-29 18:58:42 -0400

    setup basic build

---
+++ cmake-build-relwithdebinfo/BLT-Graphics/libraries/assimp/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/brett/git/BLT-Dev/BLT-Graphics/libraries/assimp/contrib/zlib
+# Build directory: /home/brett/git/BLT-Dev/cmake-build-relwithdebinfo/BLT-Graphics/libraries/assimp/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+//
+// Created by brett on 7/29/23.
+//
+#include <iostream>
+
+#include <parks/window.h>
+#include <blt/std/logging.h>
+#include <parks/renderer/engine.h>
+#include <blt/profiling/profiler.h>
+
+using namespace parks;
+
+int main(int argc, const char** argv){
+    
+    Settings settings;
+    settings.setProperty(Properties::WINDOW_WIDTH, new Properties::Value<int>(1440));
+    settings.setProperty(Properties::WINDOW_HEIGHT, new Properties::Value<int>(720));
+    settings.setProperty(Properties::WINDOW_RESIZABLE, new Properties::Value<bool>(true));
+    settings.setProperty(Properties::WINDOW_TITLE, new Properties::Value<std::string>("So You Think You Can Park?"));
+    
+    // TODO: poll hardware
+    resources::init(16);
+    // TODO local
+    resources::loadTexture("/home/brett/git/parksandrec/resources/textures/test.png", "test.png");
+    
+    Window::create(settings);
+    
+    resources::beginLoading();
+    
+    parks::Engine gameEngine(settings);
+    //Window::setMouseVisible(false);
+    gameEngine.run();
+    
+    resources::cleanup();
+    
+    BLT_INFO("Engine has stopped running. Have a nice day!");
+    
+}
