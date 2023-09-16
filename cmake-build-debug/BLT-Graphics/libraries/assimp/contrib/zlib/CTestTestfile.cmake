c33aafe8c292ffec2c04780cae5e5824f9633d63
Brett <brettmaster1@gmail.com>
2023-09-16 17:28:37 -0400

    arg parse working on

--- cmake-build-debug/BLT-Graphics/libraries/assimp/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/brett/Documents/code/c++/BLT_dev/BLT-Test/BLT-Graphics/libraries/assimp/contrib/zlib
-# Build directory: /home/brett/Documents/code/c++/BLT_dev/BLT-Test/cmake-build-debug/BLT-Graphics/libraries/assimp/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
